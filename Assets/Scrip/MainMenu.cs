cca174dc39c9a8e235531a83c1610244b181c719
secio <samson.diva@gmail.com>
2023-07-27 22:32:30 +0700

    Final Change

--- Assets/Scrip/MainMenu.cs
+++ Assets/Scrip/MainMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Progress;
 
 public class MainMenu : MonoBehaviour
 {
